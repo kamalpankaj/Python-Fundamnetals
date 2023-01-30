141373ebb34478c6f9424b8f35ad2a439269a431
Kamal Pankaj <kamal.pankaj20@gmail.com>
2023-01-30 20:42:11 +0530

    This is new  program

---
+++ 6-SetDataType.py
@@ -0,0 +1,14 @@
+#Set Data Type
+print("Set Data Type")
+print('''Set is data structure in python, that is an unordered collection
+of unique elements. Sets are used to store multiple iteam in single Variable.''')
+FirstSet={1,2,3,4,5,6,7,8,9,"kamal",1,2,3,4,5,"Pankaj","kamal","Pankaj"}
+print(FirstSet)
+# Printing the variable data type
+print(type(FirstSet))
+# Using of Remove Method 
+FirstSet.remove(1)
+print(FirstSet)
+# Using of add method
+FirstSet.add(1)
+print(FirstSet)
